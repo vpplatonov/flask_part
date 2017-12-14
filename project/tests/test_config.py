090752d524b78a3d983c70e384e128d75e1e2d85
Valerii Platonov <valeri.platonov@smtp.com>
2017-12-14 23:06:05 +0200

    initial commit

---
+++ project/tests/test_config.py
@@ -0,0 +1,55 @@
+# project/tests/test_config.py
+
+
+import unittest
+
+from project import create_app
+from flask_testing import TestCase
+
+app = create_app()
+
+
+class TestDevelopmentConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('project.config.DevelopmentConfig')
+        return app
+
+    def test_app_is_development(self):
+        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertTrue(app.config['DEBUG'] is True)
+        self.assertFalse(app is None)
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] ==
+            'postgres://postgres:postgres@localhost:5432/users_dev'
+        )
+
+
+class TestTestingConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('project.config.TestingConfig')
+        return app
+
+    def test_app_is_testing(self):
+        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertTrue(app.config['DEBUG'])
+        self.assertTrue(app.config['TESTING'])
+        self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] ==
+            'postgres://postgres:postgres@localhost:5432/users_test'
+        )
+
+
+class TestProductionConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('project.config.ProductionConfig')
+        return app
+
+    def test_app_is_production(self):
+        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertFalse(app.config['DEBUG'])
+        self.assertFalse(app.config['TESTING'])
+
+
+if __name__ == '__main__':
+    unittest.main()
